c545433dca50b9dbeb67d3e550148c264076f259
inqbs <incubass@outlook.com>
2020-08-20 14:35:12 +0900

    tested react-router

---
+++ react-router-test/src/js/pages/Archives.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Archives extends React.Component{
+	render(){
+		return (
+			<h1>Archives</h1>
+		)
+	}
+}
+
+export default Archives;
--- react-router-test/src/js/pages/Layout.js
+++ react-router-test/src/js/pages/Layout.js
@@ -1,9 +1,28 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 class Layout extends React.Component {
   render() {
     return (
-      <h1>KillerNews.net</h1>
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <ul>
+          <li>
+            <Link to='/archives' className="btn btn-danger">archives</Link>
+          </li>
+          <li>
+            <Link to='/settings' className="btn btn-success">settings</Link>
+          </li>
+          <li>
+            <Link to='/featured' className="btn btn-primary">featured</Link>
+          </li>
+          {/* <li>
+            <NavLink to='/featured' className="btn btn-primary" activeClassName='btn-info'>featured
+            </NavLink>
+          </li> */}
+        </ul>
+      </div>
     );
   }
 }
